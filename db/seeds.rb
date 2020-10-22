0a44e0be00281f12698f0b77a1267d3808a1894a
Sam <Izguerra.sam@gmail.com>
2020-10-22 14:38:12 -0500

    added to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(name: "Seeds", email: "seeds@seeds.com", password: "password")
+
+Recipe.create!([
+  {title: "Seeds", chef: "seeds", ingredients: "seeds", dirctions: "seeds", image_url: "n/a", prep_time: 10, user_id: user.id},
+  {title: "Seeds 2", chef: "seeds 2", ingredients: "seeds 2", dirctions: "seeds 2", image_url: "n/a 2", prep_time: 12, user_id: user.id}
+])
